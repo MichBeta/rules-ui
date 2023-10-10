808f9f863fd65a4992b90a33a39297540a8b5e9a
INT\Michael.Betancourt <michael.betancourt@solera.com>
2023-10-10 13:39:28 -0600

    small changes

--- src/components/rule.tsx
+++ src/components/rule.tsx
@@ -8,7 +8,7 @@ export function Rule() {
     if (error) return <div>No rule found</div>
     return (
         <>
-            <div className={"flex flex-col space-y-4 bg-zinc-800 text-white p-4 m-2 rounded-md"}>
+            <div className={"flex flex-col space-y-4 bg-zinc-800 text-white bg-gray-900 p-4 m-2 rounded-md"}>
                 <div className={"flex flex-row space-x-4"}>
                     <div className={"font-bold"}>Entity Id:</div>
                     <div>{data?.entityId}</div>
@@ -16,6 +16,10 @@ export function Rule() {
                 <div className={"flex flex-row space-x-4"}>
                     <div className={"font-bold"}>Rule Code:</div>
                     <div>{data?.ruleCode}</div>
+                </div>
+                <div className={"flex flex-row space-x-4"}>
+                    <div className={"font-bold"}>Rule Name:</div>
+                    <div>{data?.name}</div>
                 </div>
                 <div className={"flex flex-row space-x-4"}>
                     <div className={"font-bold"}>Description:</div>
