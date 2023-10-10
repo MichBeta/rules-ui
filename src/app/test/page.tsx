3cac5c4f3a23b1e71769ef8ffcfd9450f5711ec7
INT\Michael.Betancourt <michael.betancourt@solera.com>
2023-10-10 13:34:41 -0600

    tests fixes

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -30,31 +30,14 @@ function TestPage() {
                 dispatch(setRuleCode(ruleCode));
             } }>
                 <div className={"flex flex-row space-x-4 items-center"}>
-                    <div className={"font-bold text-white text-center"}>Rule Code:</div>
-                    <input className={"rounded-md"} type={"text"} name={"ruleCode"} />
+                    <div className={"font-bold text-center"}>Rule Code:</div>
+                    <input className={"rounded-md border border-black"} type={"text"} name={"ruleCode"} />
                     <button className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"} >
                         Submit
                     </button>
                 </div>
             </form>
             <Rule/>
-
-            <h2 className={"text-2xl mt-2"}>Group</h2>
-            <form className={"flex flex-col space-y-4 bg-zinc-800 p-4 m-2 rounded-md"} onSubmit={(e) => {
-                e.preventDefault();
-                const GroupCode = e.currentTarget.ruleCode.value;
-                dispatch(setGroupCode(GroupCode));
-            } }>
-                <div className={"flex flex-row space-x-4 items-center"}>
-                    <div className={"font-bold text-white text-center"}>Rule Code:</div>
-                    <input className={"rounded-md"} type={"text"} name={"ruleCode"} />
-                    <button className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"} >
-                        Submit
-                    </button>
-                </div>
-            </form>
-            <Group/>
-
         </div>
     )
 }
